1c885650a13a7f2b7f39686b7a0bbcde1cc62c62
bhuvanestc <130910193+bhuvanestc@users.noreply.github.com>
2023-04-16 16:18:41 +0200

    Add files via upload

---
+++ local_global.py
@@ -0,0 +1,5 @@
+message = 'good morning'
+def greet():
+    print("Local",message)
+greet()
+print('Global',message)
---
+++ singlemultiline.py
@@ -0,0 +1,5 @@
+#this is single line comment
+print("Don't lose hope, you don't know what tomorrow brings")
+#this is also single line comment
+print("stay positive")
+"""this is multiline comment useful to express the operation that takes place in code"""
